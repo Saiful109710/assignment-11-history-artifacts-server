8c1eb814e412761195ffe44491b4a5f0b53d0783
saiful-islam <saifulislam109710@gmail.com>
2025-01-07 13:22:34 +0600

    add sall and details artifacts api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 
 const app = express();
 
@@ -11,6 +13,48 @@ app.use(cors())
 app.use(express.json())
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.92ej0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const artifactsInfo = client.db('artifactsInfo').collection('All_Artifacts')
+
+
+    app.get('/allArtifacts',async(req,res)=>{
+        const result = await artifactsInfo.find().toArray();
+        res.send(result)
+    })
+
+    app.get('/artifactDetails/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id:new ObjectId(id)}
+        const result =await artifactsInfo.findOne(query)
+        res.send(result)
+    })
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/',async(req,res)=>{
     res.send('server is running')
 })
@@ -18,3 +62,6 @@ app.get('/',async(req,res)=>{
 app.listen(port,()=>{
     console.log(`app is running on port ${port}`)
 })
+
+// histoy_artifacts
+// x29IIJDpYXheOGbC
